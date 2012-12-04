70d649b7017f52a795fd3c6a41eff1977d40b10d
Paul Glavich <glav@theglavs.com>
2012-12-05 08:09:55 +1100

    significant performance and player handling plus some images

--- Win8-AirHockey/Views/GameBoard/worldConstants.js
+++ Win8-AirHockey/Views/GameBoard/worldConstants.js
@@ -8,5 +8,17 @@ window.game.worldConstants = {
     Player1Colour: "#2DC9CE",
     Player2Colour: "#EFB81F",
     BallPuckColour: "#3ED337",
-    GoalColour: "#E0302A"
+    GoalColour: "#E0302A",
+    PuckId: 'puck',
+    Player1Id: 'bat1',
+    Player2Id: 'bat2',
+    player1Goal: 'goal1',
+    player2Goal: 'goal2'
 };
+
+window.game.gestureType = {
+    start: 0,
+    change: 1,
+    end: 2,
+    hold: 3
+}
